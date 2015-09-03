259b0a0346a36120398f23a95c0cb2be5ab10aea
Lisa Folliard & Aileen Cacayorin <student@epicodus.com>
2015-09-03 15:55:00 -0700

    fix withdrawal function

--- app.rb
+++ app.rb
@@ -83,8 +83,13 @@ end
 post('/kids/:id') do
   id = params.fetch('id').to_i
   kid = Kid.find(id)
-  transaction_type = params.fetch('transaction_type')
   amount = params.fetch('amount').to_f
+  transaction_type = params.fetch('transaction_type')
+  if transaction_type.=="withdrawal"
+      amount = amount.abs.*(-1)
+  else
+    amount
+  end
   description = params.fetch('description')
   date = params.fetch('date')
   transaction = Transaction.create(:transaction_type => transaction_type, :kid_id => kid.id, :amount => amount, :date => date, :description => description)
@@ -167,9 +172,14 @@ end
 
 post('/parent/:id/request') do
   kid_id = params.fetch('kid_id').to_i
-  transaction_type = params.fetch('transaction_type')
-  description = params.fetch('description')
   amount = params.fetch('amount').to_f
+  transaction_type = params.fetch('transaction_type')
+  if transaction_type.=="withdrawal"
+    amount = amount.abs.*(-1)
+  else
+    amount
+  end
+  description = params.fetch('description')
   date = Date.today
   new_transaction = Transaction.create({:amount => amount, :transaction_type => transaction_type, :description => description, :date => date, :kid_id => kid_id })
   request_id = params.fetch('request_id').to_i
